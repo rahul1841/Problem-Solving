1b67e3d06a429ff8c3ae809ae50060fcce462f4a
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-03-23 13:59:44 +0000

    done

--- tree/day1,2.cpp
+++ tree/day1,2.cpp
@@ -113,3 +113,49 @@ class Solution{
       return oddSum-evenSum;
     }
 };
+
+
+Q4: level order traversel
+
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, map< int, vector<int> > > nodes;
+        queue< pair<TreeNode*, pair<int, int> > > q;
+        vector<vector<int>> ans;
+        if(root == NULL){
+            return ans;
+        }
+        
+        q.push(make_pair(root, make_pair(0, 0)));
+        
+        while(!q.empty()){
+            pair<TreeNode*, pair<int, int> > temp = q.front();
+            q.pop();
+            TreeNode* frontNode = temp.first;
+            int hd = temp.second.first;
+            int lvl = temp.second.second;
+            
+            nodes[hd][lvl].push_back(frontNode->val);
+            
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left, make_pair(hd-1, lvl+1)));
+            }
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right, make_pair(hd+1, lvl+1)));
+            }
+        }
+        for(auto i : nodes){
+            vector<int> temp;
+            for(auto j : i.second){
+                sort(j.second.begin(), j.second.end());
+                for(auto k : j.second){
+                    temp.push_back(k);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
