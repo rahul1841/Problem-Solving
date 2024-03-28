32f5e389024b4f412f34a782f4d80d85122a4eca
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-03-28 20:35:56 +0000

    tillq7done

--- tree/day3.cpp
+++ tree/day3.cpp
@@ -68,3 +68,158 @@ public:
         return ans;
     }
 };
+
+
+Q4: Path to Given Node
+
+bool helper(TreeNode* root, int B, vector<int> &ans){
+    if (root == NULL) {
+        return false;
+    }
+    ans.push_back(root->val);
+
+    if (root->val == B) {
+        return true;
+    }
+
+    if (helper(root->left, B, ans) || helper(root->right, B, ans)) {
+        return true;
+    }
+
+    ans.pop_back();
+    return false;
+     
+ }
+ 
+vector<int> Solution::solve(TreeNode* A, int B) {
+    vector<int> ans;
+    helper(A,B,ans);
+    return ans;
+}
+
+
+Q5: LCA
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL){
+            return NULL;
+        }
+        
+        if(root->val == p->val || root->val == q->val){
+            return root;
+        }
+        
+     TreeNode*   leftAns = lowestCommonAncestor(root->left,p,q);
+      TreeNode*  rightAns = lowestCommonAncestor(root->right,p,q);
+        
+        if(leftAns != NULL && rightAns != NULL){
+            return root;
+        }
+        else if(leftAns){
+            return leftAns;
+        }
+        else if(rightAns){
+            return rightAns;
+        }
+        else{
+            return NULL;
+        }
+    }
+};
+
+
+Q6 : Invert Binary TreeNode
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+        
+        return root;
+        
+    }
+};
+
+
+Q7: Diameter of Binary Tree
+
+App1:- 
+class Solution {
+    private:
+    int height(TreeNode* root){
+        if(root ==  NULL){
+            return 0;
+        }
+        
+        int left = height(root->left);
+        int right = height(root->right);
+        
+        int ans = max(left,right) + 1;
+        return ans;
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        
+        int opt1 = diameterOfBinaryTree(root->left);
+        int opt2 = diameterOfBinaryTree(root->right);
+        int opt3 = height(root->left) + height(root->right);
+        
+        int ans = max(opt1,max(opt2,opt3));
+        return ans;
+    }
+};
+
+App2:-
+class Solution {
+    private:
+    int height(TreeNode* root){
+        if(root ==  NULL){
+            return 0;
+        }
+        
+        int left = height(root->left);
+        int right = height(root->right);
+        
+        int ans = max(left,right) + 1;
+        return ans;
+    }
+public:
+    pair<int,int> diameterFast(TreeNode* root){
+        if(root == NULL){
+            pair<int,int> p = make_pair(0,0);
+            return p;
+        }
+        
+        pair<int,int> left = diameterFast(root->left);
+        pair<int,int> right = diameterFast(root->right);
+        
+        int opt1 = left.first;
+        int opt2 = right.first;
+        int opt3 = left.second + right.second ;
+        
+        pair<int,int> ans;
+        ans.first = max(opt1,max(opt2,opt3));
+        ans.second = max(left.second , right.second) + 1;
+        
+        return ans;
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        return diameterFast(root).first;
+
+    }
+};
