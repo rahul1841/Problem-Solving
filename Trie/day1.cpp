347c18de765839fd2c2736be91110b1f88c7af8b
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-04-17 11:00:50 +0000

    done

--- Trie/day1.cpp
+++ Trie/day1.cpp
@@ -70,7 +70,92 @@ public:
     }
 };
 
-Q2: Replace word
+Q2: Sum of Prefix Scores of Strings
+
+struct Node{
+    Node* links[26];
+    int prefix = 0;
+    
+    bool containsKey(char ch){
+        return (links[ch-'a'] != NULL);
+    }
+    
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+    
+    void put(char ch, Node* node){
+        links[ch-'a'] = node;
+    }
+    
+    void incPrefix(){
+       prefix ++; 
+    }
+    
+    int getPrefix(){
+        return prefix;
+    }
+};
+
+class Trie{
+    
+    private:
+    
+    Node* root;
+    
+    public:
+    
+    Trie(){
+        root = new Node();
+    }
+    
+    void insert (string &word){
+        Node* node = root;
+        for(int i = 0; i<word.size(); i++){
+        if(!node->containsKey(word[i])){
+            node->put(word[i], new Node());
+        }
+            node = node->get(word[i]);
+            node->incPrefix();
+        }
+    }
+    
+    int countPrefix(string &word){
+        Node* node = root;
+        int ans = 0;
+        for(int i = 0; i<word.size(); i++){
+            if(node->containsKey(word[i])){
+                node = node->get(word[i]);
+                ans += node->getPrefix();
+            }else{
+                break;
+            }
+        }
+        return ans;
+    }
+    
+};
+
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        int n = words.size();
+        vector<int> ans(n);
+        Trie trie;
+        for(int i = 0 ; i< n; i++){
+            trie.insert(words[i]);
+        }
+        for(int i = 0; i<n; i++){
+            ans[i] = trie.countPrefix(words[i]);
+        }
+        return ans;
+    }
+};
+
+
+
+
+Q3: Replace word
 
 #include <bits/stdc++.h>
 using namespace std;
