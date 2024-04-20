691f278f7d3d4799d8e6ad2b84f77d55f715018c
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-04-20 20:23:30 +0000

    commit

--- graph/day1.cpp
+++ graph/day1.cpp
@@ -29,3 +29,28 @@ public:
 
     }
 };
+
+Q3 number-of-provinces
+
+class Solution {
+public:
+    void dfs(int i,vector<vector<int>>& isConnected,vector<int>&vis){
+        vis[i]=1;
+        for(int j=0;j<isConnected.size();j++){
+            if(isConnected[i][j]==1 && vis[j]==0){
+                dfs(j,isConnected,vis);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        vector<int>vis(isConnected.size(),0);
+        int cnt=0;
+        for(int i=0;i<isConnected.size();i++){
+            if(vis[i]==0){
+                cnt++;
+                dfs(i,isConnected,vis);
+            }
+        }
+        return cnt;
+    }
+};
