727cd737f17015c69e0dc3481a823000e94fbf07
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-04-21 12:46:46 +0000

    commit

--- graph/day1.cpp
+++ graph/day1.cpp
@@ -54,3 +54,38 @@ public:
         return cnt;
     }
 };
+
+Q4 detect-cycle-in-a-directed-graph
+
+class Solution {
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        int inorder[V]={0};
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                inorder[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(inorder[i]==0){
+                q.push(i);
+            }
+        }
+        int cnt=0;
+        while(!q.empty()){
+            int front=q.front();
+            q.pop();
+            cnt++;
+            for(auto it:adj[front]){
+                inorder[it]--;
+                if(inorder[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(cnt==V) return false;
+        return true;
+    }
+};
