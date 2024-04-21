7fe6679b836662ff80bbe5e6bf013e6fb032d325
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-04-21 19:03:38 +0000

    commit

--- graph/day1.cpp
+++ graph/day1.cpp
@@ -89,3 +89,28 @@ class Solution {
         return true;
     }
 };
+
+Q5 minimum-number-of-vertices-to-reach-all-nodes
+
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int>indeg(n,0);
+        vector<int>adj[n];
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+        }
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                indeg[it]++;
+            }
+        }
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(indeg[i]==0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
