88fb34de18e215503d27d60cafdfba2e4f153112
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-05-02 16:54:40 +0000

    commit

--- graph/day3.cpp
+++ graph/day3.cpp
@@ -79,6 +79,37 @@ public:
     }
 };
 
+Q4 all-ancestors-of-a-node-in-a-directed-acyclic-graph
+
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<int>adj[n];
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+        }
+        vector<vector<int>>ans(n);
+        for(int node=0;node<n;node++){
+            vector<int>vis(n,0);
+            queue<int>q;
+            q.push(node);
+            vis[node]=1;
+            while(!q.empty()){
+                int currnode=q.front();
+                q.pop();
+                for(auto it:adj[currnode]){
+                    if(!vis[it]){
+                        vis[it]=1;
+                        q.push(it);
+                        ans[it].push_back(node);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
 Q6 detect-cycle-in-a-directed-graph
 
 class Solution {
