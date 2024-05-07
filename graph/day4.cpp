e7531ee078c9b4fca2a40fcf1529b72c546f2496
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-05-07 19:12:24 +0000

    commit

--- graph/day4.cpp
+++ graph/day4.cpp
@@ -85,6 +85,65 @@ public:
     }
 };
 
+Q3 clone-graph
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* dfs(Node* curr, Node* node, vector<Node*>& vis) {
+        vis[curr->val] = node;
+        for (auto it : curr->neighbors) {
+            if (vis[it->val] == NULL) {
+                Node* newnode = new Node(it->val);
+                (node->neighbors).push_back(newnode);
+                dfs(it, newnode, vis);
+            } else {
+                (node->neighbors).push_back(vis[it->val]);
+            }
+        }
+        return node;
+    }
+    Node* cloneGraph(Node* node) {
+        if (node == NULL) {
+            return NULL;
+        }
+        vector<Node*> vis(1000, NULL);
+        Node* copy = new Node(node->val);
+        vis[node->val] = copy;
+
+        for (auto curr : node->neighbors) {
+            if (vis[curr->val] == NULL) {
+                Node* newnode = new Node(curr->val);
+                (copy->neighbors).push_back(newnode);
+                dfs(curr, newnode, vis);
+            } else {
+                (copy->neighbors).push_back(vis[curr->val]);
+            }
+        }
+        return copy;
+    }
+};
+
 Q4 shortest-cycle-in-a-graph
 
 class Solution {
