3aedb7b83cb7e9503e4bfe6027c1db56d07335cf
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-05-09 10:52:34 +0000

    commit

--- graph/day5.cpp
+++ graph/day5.cpp
@@ -71,3 +71,73 @@ public:
         return dist[end_node];
     }
 };
+
+Q3 network-delay-time
+
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
+
+        vector<pair<int,int>>adj[n+1];
+        for(auto it:times){
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        vector<int> dist(n+1,INT_MAX);
+        dist[k]=0;
+        while(!pq.empty()){
+            int dis=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            for(auto it:adj[node]){
+                int wt=it.second;
+                int adjnode=it.first;
+                if(dist[adjnode]>dis+wt){
+                    dist[adjnode]=dis+wt;
+                    pq.push({dist[adjnode],adjnode});
+                }
+            }
+        }
+        int mx=*max_element(dist.begin()+1,dist.end());
+        return mx==INT_MAX?-1:mx;
+    }
+};
+
+Q4 path-with-minimum-effort
+
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> > pq;
+        pq.push({0,{0,0}});
+        //{dis,{row,col}}
+        int n=grid.size();
+        int m=grid[0].size();
+
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+
+        int dr[4]={-1,0,1,0};
+        int dc[4]={0,1,0,-1};
+        while(!pq.empty()){
+            int dis=pq.top().first;
+            int row=pq.top().second.first;
+            int col=pq.top().second.second;
+            pq.pop();
+            if(row==n-1 && col==m-1) return dis;
+            for(int i=0;i<4;i++){
+                int nrow=row+dr[i];
+                int ncol=col+dc[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                    int newdis=max(dis,abs(grid[nrow][ncol]-grid[row][col]));
+                    if(newdis<dist[nrow][ncol]){
+                        dist[nrow][ncol]=newdis;
+                        pq.push({newdis,{nrow,ncol}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
