ce7e4e4c04e84414178f864d286632b00004698e
Rahul Kumar <118290059+rahul1841@users.noreply.github.com>
2024-05-20 20:41:30 +0000

    commit

--- DP/day-3.cpp
+++ DP/day-3.cpp
@@ -76,6 +76,22 @@ public:
 
 Q4 maximum-product-subarray
 
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>dp(n , vector<int>( 2, nums[0]));
+        int ans=nums[0];
+        for(int i=1;i<n;i++){
+            dp[i][0]=max(dp[i-1][0]*nums[i]  , max( dp[i-1][1]*nums[i]  , nums[i] ));
+            dp[i][1]=min( dp[i-1][0]*nums[i] , min( dp[i-1][1]*nums[i] , nums[i] ));
+            ans = max(ans , max(dp[i][1] , dp[i][0]));
+        }
+        return ans; 
+
+    }
+};
+
 Q5 longest-increasing-subsequence
 
 class Solution {
